627c7e4d97e6a44cabac63aaee0142afc757a7be
Elie Schoppik <eschoppik@gmail.com>
2015-06-15 23:23:13 -0700

    adding env parts

--- app.js
+++ app.js
@@ -139,6 +139,6 @@ app.get('*', function(req,res){
   res.render('404');
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server is listening on port 3000");
 });
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/country_app_with_auth");
-mongoose.set("debug", true)
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/country_app_with_auth");
+mongoose.set("debug", true);
 
 module.exports.User = require("./user");
 module.exports.Country = require("./country");
